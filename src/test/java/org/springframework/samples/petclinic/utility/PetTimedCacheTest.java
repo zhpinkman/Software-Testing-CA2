4e439618e3652c5811613eb17ec69f3edf52676b
zhivar <zhivarsourati@gmail.com>
2020-10-26 12:58:13 +0330

    Add save and init test to PetTimedCacheTest

--- src/test/java/org/springframework/samples/petclinic/utility/PetTimedCacheTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PetTimedCacheTest.java
@@ -1,8 +1,43 @@
 package org.springframework.samples.petclinic.utility;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.samples.petclinic.owner.Pet;
+import org.springframework.samples.petclinic.owner.PetRepository;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
-// todo
+
 class PetTimedCacheTest {
+
+	@Mock
+	private PetRepository petRepository;
+
+	private PetTimedCache petTimedCache;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		petTimedCache = new PetTimedCache(petRepository);
+	}
+
+	@Test
+	public void initTest() {
+		assertNotNull(petTimedCache);
+		assertNotNull(petRepository);
+	}
+
+	@Test
+	public void saveTest() {
+		Pet petInstance = new Pet();
+		petTimedCache.save(petInstance);
+		Mockito.verify(petRepository, Mockito.times(1)).save(petInstance);
+	}
+
 }
